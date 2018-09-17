6b0fa82c69333370d4c62a1ddc8eb31557f3560b
SamuelV3loso <samuelveloso42@gmail.com>
2018-09-17 10:48:42 -0300

    Alteração de caminho do banco de dados

--- mousefeast/src/main/java/com/amazon/mousefeast/repository/ProdutoRepository.java
+++ mousefeast/src/main/java/com/amazon/mousefeast/repository/ProdutoRepository.java
@@ -1,9 +1,11 @@
 package com.amazon.mousefeast.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.amazon.mousefeast.models.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
--- mousefeast/src/main/java/com/amazon/mousefeast/resources/ProdutoResources.java
+++ mousefeast/src/main/java/com/amazon/mousefeast/resources/ProdutoResources.java
@@ -3,7 +3,12 @@ package com.amazon.mousefeast.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,26 @@ public class ProdutoResources {
 	@GetMapping("/produtos")
 	public List<Produto> listaProdutos(){
 		 return produtoRepository.findAll();
+	}
+	
+
+	@GetMapping
+	public List<Produto> ListarPeloId() {
+		return produtoRepository.findAll();
+	}
+
+	@PostMapping
+	public Produto salvar(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@PutMapping
+	public Produto atualizar(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+
+	@DeleteMapping("{id}")
+	public void deletarPeloId(@PathVariable("id") Long id) {
+		produtoRepository.deleteById(id);
 	}
 }
