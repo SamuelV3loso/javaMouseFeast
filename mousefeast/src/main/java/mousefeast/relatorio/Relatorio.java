80a4c005a6ef5f4bbae327e713215da9f908120e
Samuel Veloso <samuelveloso42@gmail.com>
2018-10-01 11:36:20 -0300

    Adição de métodos na classe RelatorioTest.java

--- mousefeast/src/main/java/mousefeast/relatorio/Relatorio.java
+++ mousefeast/src/main/java/mousefeast/relatorio/Relatorio.java
@@ -1,5 +1,8 @@
 package mousefeast.relatorio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.amazon.mousefeast.models.Produto;
@@ -11,20 +14,39 @@ public class Relatorio {
 	private Double maiorValor = Double.NEGATIVE_INFINITY;
 	private Double menorValor = Double.POSITIVE_INFINITY;
 
+	private List<Produto> produtosMaisCaros;
+	
+	public List<Produto> getProdutosMaisCaros() {
+		return produtosMaisCaros;
+	}
+
 	public void gerarRelatorioPrecos(List<Produto> produtos) {
 		for (Produto produto : produtos) {
-			if (produto.getValor() > maiorValor) {
+			if ( produto.getValor() > maiorValor ) {
 				maiorValor = produto.getValor();
 			}
 
-			if (produto.getValor() < menorValor) {
+			if ( produto.getValor() < menorValor ) {
 				menorValor = produto.getValor();
 			}
 		}
-
+		calcularProdutosMaisCaros(produtos);
 	}
 	
-	
+	private void calcularProdutosMaisCaros(List<Produto> produtos) {
+		
+		produtosMaisCaros = new ArrayList<>(produtos);
+		Collections.sort(produtosMaisCaros, new Comparator<Produto>() {
+			public int compare(Produto p1, Produto p2) {
+				if(p1.getValor() < p2.getValor()) return 1;
+				if(p1.getValor() > p2.getValor()) return -1;
+				return 0;			}
+		});
+		
+		produtosMaisCaros = produtosMaisCaros.subList(0, 
+				produtosMaisCaros.size() > 3 ? 3 : produtosMaisCaros.size());
+		
+	}
 
 	public Double getMaiorValor() {
 		return maiorValor;
--- mousefeast/src/test/java/mousefeast/relatorio/RelatorioTest.java
+++ mousefeast/src/test/java/mousefeast/relatorio/RelatorioTest.java
@@ -3,6 +3,7 @@ package mousefeast.relatorio;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -10,18 +11,26 @@ import com.amazon.mousefeast.models.Produto;
 
 public class RelatorioTest {
 
-	@Test
-	public void deveEncontrarOMaiorEMenorPrecoDosProdutosEmOrdemCrescente() {
-		Produto celular = new Produto( "Eletrônicos", "Moto G500", 250.00 );
+	private Relatorio relatorio;
+	private List<Produto> produtos;
+	
+	@Before
+	public void criarEntidadesParaTestes() {
+		relatorio = new Relatorio();
 		Produto tv = new Produto( "Eletrônicos", "Samsung HD", 300.00 );
 		Produto dvd = new Produto( "Eletrônicos", "CCE DVD", 400.0 );
+		Produto celular = new Produto( "Eletrônicos", "Moto G500", 250.00 );
 		
-		List<Produto> produtos = new ArrayList<> ();
-		produtos.add(celular);
+		produtos = new ArrayList<> ();
 		produtos.add(tv);
 		produtos.add(dvd);
+		produtos.add(celular);
 		
-		Relatorio relatorio = new Relatorio();
+	}
+	
+	@Test
+	public void deveEncontrarOMaiorEMenorPrecoDosProdutosEmOrdemCrescente() {
+		
 		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorValorEsperado=400.00;
@@ -33,17 +42,7 @@ public class RelatorioTest {
 	
 	@Test
 	public void deveEncontrarOMaiorEMenorPrecoDosProdutosEmOrdemDecrescente() {
-
-		Produto dvd = new Produto( "Eletrônicos", "CCE DVD", 400.0 );
-		Produto tv = new Produto( "Eletrônicos", "Samsung HD", 300.00 );
-		Produto celular = new Produto( "Eletrônicos", "Moto G500", 250.00 );
 		
-		List<Produto> produtos = new ArrayList<> ();
-		produtos.add(dvd);
-		produtos.add(tv);
-		produtos.add(celular);
-		
-		Relatorio relatorio = new Relatorio();
 		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorValorEsperado=400.00;
@@ -55,18 +54,6 @@ public class RelatorioTest {
 	
 	@Test
 	public void deveEncontrarOMaiorEMenorPrecoDosProdutosEmOrdemAleatoria() {
-
-		
-		Produto tv = new Produto( "Eletrônicos", "Samsung HD", 300.00 );
-		Produto dvd = new Produto( "Eletrônicos", "CCE DVD", 400.0 );
-		Produto celular = new Produto( "Eletrônicos", "Moto G500", 250.00 );
-				
-		List<Produto> produtos = new ArrayList<> ();
-		produtos.add(tv);
-		produtos.add(dvd);
-		produtos.add(celular);
-		
-		Relatorio relatorio = new Relatorio();
 		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorValorEsperado=400.00;
@@ -79,22 +66,28 @@ public class RelatorioTest {
 	@Test
 	public void deveEncontrarOMaiorEMenorPrecoDeUmUnicoProduto() {
 
-		
-		Produto tv = new Produto( "Eletrônicos", "Samsung HD", 300.00 );
-				
-		List<Produto> produtos = new ArrayList<> ();
-		produtos.add(tv);
-		
-		Relatorio relatorio = new Relatorio();
 		relatorio.gerarRelatorioPrecos(produtos);
 		
-		Double maiorValorEsperado=300.00;
-		Double menorValorEsperado=300.00;
+		Double maiorValorEsperado=400.00;
+		Double menorValorEsperado=400.00;
 		
 		assertEquals(maiorValorEsperado, relatorio.getMaiorValor());
 		assertEquals(menorValorEsperado, relatorio.getMenorValor());
 	}
 	
-	
-	
+	@Test
+	public void deveEncontrarOsTresProdutosMaisCaros() {
+		
+		Produto afonso = new Produto( "RH", "Afonso", 70.00 );
+		produtos.add(afonso);
+		
+		relatorio.gerarRelatorioPrecos(produtos);
+		
+		List<Produto> top3ProdutosMaisCaros = relatorio.getProdutosMaisCaros();
+		
+		assertEquals(3, top3ProdutosMaisCaros.size());
+		assertEquals(400.00, top3ProdutosMaisCaros.get(0).getValor(), 0.00001);
+		assertEquals(300.00, top3ProdutosMaisCaros.get(1).getValor(), 0.00001);
+		assertEquals(250.00, top3ProdutosMaisCaros.get(2).getValor(), 0.00001);
+	}	
 }
